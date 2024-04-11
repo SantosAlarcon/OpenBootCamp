6ca06c697331eaff1d7376e8e805870aaa0da0cb
Santos Alarcón Asensio <santosalarcon86@gmail.com>
2024-04-11 10:34:51 +0200

    Update RegisterFormMaterial.tsx

--- MERN/Ejercicio13-Front/src/components/forms/RegisterFormMaterial.tsx
+++ MERN/Ejercicio13-Front/src/components/forms/RegisterFormMaterial.tsx
@@ -18,10 +18,6 @@ export default function RegisterFormMaterial() {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
   };
 
   return (
